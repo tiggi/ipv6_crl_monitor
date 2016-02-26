4b33583f2f9913a373793243ed31e84ec75c08dd
Ulf Tigerstedt <tigerstedt@iki.fi>
2016-02-26 14:32:50 +0200

    Removed commented-out debugging, added comments

--- getstat.php
+++ getstat.php
@@ -1,4 +1,6 @@
 <?php
+// Code by Ulf Tigerstedt <tigerstedt@iki.fi> for the HEPIX IPv6 WG
+
 date_default_timezone_set("Europe/Helsinki");
 
 
@@ -12,29 +14,20 @@ $db=new SQLite3("ipv6.sqlite");
 foreach ($files as $a) {
 
 	$temp = explode('/',$a);
-	//print_r($temp);
 	// $temp[4] filename
 	$ca_base = strtok($temp[4],'.');
-	//print_r($ca);
 	$ret_tuple=check_aaaa(crl_hostnames($a));
 
 	foreach ($ret_tuple as $current) {
-//		print_r($current);
 		insert_database($ca_base,$current[0],$current[1]);
 	}
-
-	#print_r($ret_tuple);
-	
 
 } 
 
 function crl_hostnames($crl_url) {
 	$urls=file($crl_url);
-	//print_r($urls);
 	foreach ($urls as $now) {
 		$ret=preg_match("/http:\/\/([a-z0-9.-]{2,})\/.*/",$now,$arr);
-		//checkdnsrr($host,"AAAA");
-		//print_r($arr);
 		if ($ret == 1) {
 			$hostnames[]=$arr[1];
 		}
@@ -48,18 +41,18 @@ function check_aaaa($hostnames) {
 
 		$dns_arr=dns_get_record($host,DNS_AAAA);
 		if (count($dns_arr)>=1) {
-		//	print($host . " has AAAA\n");
-		//	print_r($dns_arr);
 			foreach ($dns_arr as $multiple) {
 				$ret=check_connectivity($multiple["ipv6"]);
 				if ($ret) {
+					// Has AAAA and connection to it works
 					$retarr[]=Array($host,"works");
 				} else {
+					// Has AAAA but this host can't connect
 					$retarr[]=Array($host,"AAAA");
 				}
 			}
 		} else {
-		//	print($host . " is IPv4 only\n");
+				// Only has A record
 				$retarr[]=Array($host,"ipv4");
 		}
 	}
@@ -68,6 +61,7 @@ function check_aaaa($hostnames) {
 }
 
 function check_connectivity($ipv6) {
+	// Try opening a TCP-connection to the IPv6 address on port 80
 	$fp = fsockopen("[" . $ipv6 . "]",80,$errno,$srrstr,10);
 	
 	if (!$fp) {
@@ -76,7 +70,7 @@ function check_connectivity($ipv6) {
 		fclose($fp);
 		return true;
 	}
-
+	// This statement is wrong
 	return;
 }
 
