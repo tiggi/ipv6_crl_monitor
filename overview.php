00789f5e2f005afabe1b308ceeafe004776057d4
Ulf Tigerstedt <tigerstedt@iki.fi>
2016-03-15 11:23:10 +0200

    Fixing missing php tags

--- overview.php
+++ overview.php
@@ -1,7 +1,21 @@
-<?
+<?php
 
 require('reports.php');
+$db=db_open("ipv6.sqlite");
 
 
+$dates=get_dates();
+
+
+print_r($dates);
+
+
+function print_overview($dates) {
+
+	print("<table>\n");
+	print("<tr><th>Date</th><th>Good</th><th>Trying</th><th>Broken</th></tr>\n");
+	print("</table>\n");
+
+}
 
 ?>
